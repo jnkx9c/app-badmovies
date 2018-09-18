26fdce30079ce74f6f0493cab38a8d7622a4e953
Jeff <Jeff@Cindy-PC>
2018-09-18 16:25:42 -0500

    added a test method for demonstration

--- badmovies-web/src/test/java/org/kilgore/badmovies/BadmoviesWebApplicationTests.java
+++ badmovies-web/src/test/java/org/kilgore/badmovies/BadmoviesWebApplicationTests.java
@@ -1,16 +1,43 @@
 package org.kilgore.badmovies;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kilgore.badmovies.entity.Order;
+import org.kilgore.badmovies.entity.OrderItem;
+import org.kilgore.badmovies.repo.OrderRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class BadmoviesWebApplicationTests {
 
-	//@Test
+	@Autowired
+	private OrderRepo orderRepo;
+	
+	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	@Transactional
+	public void testOrder() {
+		Optional<Order> optional = orderRepo.findById(5);
+		if(optional.isPresent()) {
+			System.out.println(optional.get().getOrderDate());	
+			Order order = optional.get();
+			List<OrderItem> orderItems = order.getOrderItems();
+			for(OrderItem oi: orderItems) {
+				System.out.println("     "+oi.getId());
+			}
+		}
+		
 	}
 
 }
